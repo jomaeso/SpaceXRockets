6ed7caa3d615c1d40397a2cd98c0c43f7030448b
Jose <j.maeso@insight-solutions.es>
2021-10-19 16:21:14 +0200

    Added interactor and dependencies to Application

---
+++ app/src/main/java/com/josemaeso/spacexrockets/SpaceXRocketsApplication.kt
@@ -0,0 +1,30 @@
+package com.josemaeso.spacexrockets
+
+import android.app.Application
+import com.josemaeso.spacexrockets.data.RoomDatabase
+import com.josemaeso.spacexrockets.data.rocket.RocketRepository
+import com.josemaeso.spacexrockets.domain.rocket.RocketInteractor
+import com.josemaeso.spacexrockets.domain.rocket.RocketInteractorImpl
+import com.josemaeso.spacexrockets.domain.rocket.loader.HttpRocketLoader
+import com.josemaeso.spacexrockets.domain.rocket.loader.api.SpaceXApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SpaceXRocketsApplication : Application() {
+    lateinit var rocketInteractor: RocketInteractor
+    override fun onCreate() {
+        super.onCreate()
+
+        rocketInteractor = RocketInteractorImpl(
+            HttpRocketLoader(retrofitLoader()),
+            RocketRepository(RoomDatabase.getDatabase(applicationContext).rocketDao())
+        )
+    }
+
+    private fun retrofitLoader(): SpaceXApiService {
+        val retrofit = Retrofit.Builder().baseUrl("https://api.spacexdata.com").addConverterFactory(
+            GsonConverterFactory.create())
+            .build()
+        return retrofit.create(SpaceXApiService::class.java)
+    }
+}
