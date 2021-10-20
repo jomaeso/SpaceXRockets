82a063ffdcb01cae46b954bd20ff44e9a4a7630a
Jose <j.maeso@insight-solutions.es>
2021-10-20 13:40:30 +0200

    Added action bar with up button

--- app/src/main/java/com/josemaeso/spacexrockets/MainActivity.kt
+++ app/src/main/java/com/josemaeso/spacexrockets/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.josemaeso.spacexrockets
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.josemaeso.spacexrockets.ui.list.ListFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+                || super.onSupportNavigateUp()
+    }
+
 }
