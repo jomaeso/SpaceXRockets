4118fe64a9eeff1a078faaa32a6e5666ef7703af
Jose <j.maeso@insight-solutions.es>
2021-10-26 12:03:38 +0200

    Added tests to HttpLoader

--- app/src/test/java/com/josemaeso/spacexrockets/HttpRocketLoaderTest.kt
+++ app/src/test/java/com/josemaeso/spacexrockets/HttpRocketLoaderTest.kt
@@ -4,7 +4,9 @@ import com.josemaeso.spacexrockets.data.rocket.loader.HttpRocketLoader
 import com.josemaeso.spacexrockets.data.rocket.loader.api.SpaceXApiService
 import com.josemaeso.spacexrockets.domain.rocket.model.RocketMapper
 import kotlinx.coroutines.runBlocking
+import okhttp3.ResponseBody
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -32,6 +34,74 @@ class HttpRocketLoaderTest {
         val remoteRockets = sut.loadRockets()
 
         assertEquals(remoteRocketsAPI.map { it.id }, remoteRockets.map { it.rocketId })
+    }
+
+    @Test
+    fun test_listRockets_successEmpty() = runBlocking {
+        whenever(apiServiceMock.listRockets()).thenReturn(Response.success(listOf()))
+        val sut = makeSUT(apiServiceMock)
+
+        val remoteRockets = sut.loadRockets()
+
+        assertTrue(remoteRockets.isEmpty())
+    }
+
+    @Test
+    fun test_listRockets_error() = runBlocking {
+
+        whenever(apiServiceMock.listRockets()).thenReturn(
+            Response.error(
+                400,
+                ResponseBody.create(null, "")
+            )
+        )
+        val sut = makeSUT(apiServiceMock)
+
+        val remoteRockets = sut.loadRockets()
+
+        assertTrue(remoteRockets.isEmpty())
+    }
+
+    @Test
+    fun test_loadRocket_success() = runBlocking {
+        val rocketId = "remoteId"
+        val remoteRocketAPI =
+            RemoteRocketTestUtils.createRemoteRocketAPI(rocketId = rocketId)
+
+
+        whenever(apiServiceMock.getRocket(rocketId)).thenReturn(Response.success(remoteRocketAPI))
+        val sut = makeSUT(apiServiceMock)
+
+        val remoteRocket = sut.loadRocket(rocketId)
+
+        assertEquals(rocketId, remoteRocket?.rocketId)
+    }
+
+    @Test
+    fun test_loadRocket_successEmpty() = runBlocking {
+        val rocketId = "remoteId"
+        whenever(apiServiceMock.getRocket(rocketId)).thenReturn(Response.success(null))
+        val sut = makeSUT(apiServiceMock)
+
+        val remoteRocket = sut.loadRocket(rocketId)
+
+        assertEquals(null, remoteRocket)
+    }
+
+    @Test
+    fun test_loadRocket_error() = runBlocking {
+        val rocketId = "remoteId"
+        whenever(apiServiceMock.getRocket(rocketId)).thenReturn(
+            Response.error(
+                400,
+                ResponseBody.create(null, "")
+            )
+        )
+        val sut = makeSUT(apiServiceMock)
+
+        val remoteRocket = sut.loadRocket(rocketId)
+
+        assertEquals(null, remoteRocket)
     }
 
     private fun makeSUT(apiService: SpaceXApiService): HttpRocketLoader {
